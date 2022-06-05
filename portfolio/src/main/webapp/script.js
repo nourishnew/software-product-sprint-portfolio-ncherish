980fc0374791d2ace964acb0a0fdf3aefd5169b2
Nourish Cherish <nourishnew@gmail.com>
2022-06-05 07:42:57 +0000

    changed index.html style.css file tp update portfolio

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -17,7 +17,7 @@
  */
 function addRandomGreeting() {
   const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+      ['I love you 3000', 'BTS', 'Messi > CR7', 'Nothing. Have a good day'];
 
   // Pick a random greeting.
   const greeting = greetings[Math.floor(Math.random() * greetings.length)];
