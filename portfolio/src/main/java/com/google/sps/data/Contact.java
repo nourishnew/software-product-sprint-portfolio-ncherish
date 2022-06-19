c831567497475b8f5e352d61b98d92fccbfb0d05
Nourish Cherish <nourishnew@gmail.com>
2022-06-19 15:24:29 +0000

    added feature to load contact list

---
+++ portfolio/src/main/java/com/google/sps/data/Contact.java
@@ -0,0 +1,14 @@
+package com.google.sps.data;
+
+public final class Contact {
+  private final String name;
+  private final String email;
+  private final String other;
+
+  public Contact(String name, String email, String other) {
+    this.name = name;
+    this.email = email;
+    this.other = other;
+  }
+  
+}
